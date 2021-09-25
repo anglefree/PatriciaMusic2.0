a629f4f46de026995476af0d19d1db65aa36d4b9
anglefree <76203435+anglefree@users.noreply.github.com>
2021-09-25 07:03:58 +0530

    Ytsearsh

--- modules/ytsearch.py
+++ modules/ytsearch.py
@@ -21,7 +21,7 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search needs an argument!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("ruko jara sabar karo 😈....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
